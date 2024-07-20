38b314d639ca08b9cfbae7f2770c1aae40976655
Yasufumi Kamiyama <flum.ym@gmail.com>
2024-07-20 06:06:30 +0000

    chore: Update table frame styling in base.tsx

--- src/table/base.tsx
+++ src/table/base.tsx
@@ -103,7 +103,7 @@ function BaseTable<T extends DataRecord>(props: TPropsTable<T>) {
 
       <div
         id="table-frame"
-        className="relative w-fit h-full max-w-full max-h-[80vh] border border-gray-200 rounded-md overflow-auto"
+        className="relative h-full max-w-full max-h-[80vh] border border-gray-200 rounded-md overflow-auto"
       >
         <table className={`table`}>
           <thead>
